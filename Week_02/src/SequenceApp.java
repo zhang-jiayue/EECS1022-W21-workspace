2258a7057580d813ff457fae970f1c02ec762aff
Jiayue Zhang <69318364+zhang-jiayue@users.noreply.github.com>
2021-01-23 11:04:00 +0800

    week 2 tutorial & lab1

---
+++ Week_02/src/SequenceApp.java
@@ -0,0 +1,26 @@
+package console_apps;
+
+import java.util.Scanner;
+
+public class SequenceApp {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		// Stage 1: Prompt the user
+		System.out.println("Enter the first term of(FT) of a arithmatic seq. of size 5");
+		int ft = input.nextInt();
+		System.out.println("Enter the common difference (d):");
+		int d = input.nextInt();
+		
+		// Stage 2: Compute the result
+		
+		// Stage 3: Output the result to console
+		
+		
+		
+		input.close();
+
+	}
+
+}
